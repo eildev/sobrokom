78ca2fb0d42ea0aa35ce3aa88bd5b4bd5601abd5
majid0177 <majid.bd905@gmail.com>
2024-01-10 14:09:18 +0600

    something

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('backend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -90,3 +91,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+require __DIR__ . '/frontend.php';
